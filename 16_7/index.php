65e15bdeee8d02e803440266b0251a95ebe64e37
TienHanh2903 <63018227+TienHanh2903@users.noreply.github.com>
2021-07-19 10:07:14 +0700

    Add files via upload

--- 16_7/index.php
+++ 16_7/index.php
@@ -1,4 +1,5 @@
 <html>
+    <head><link href="style.css" rel="stylesheet"></head>
 <body>
 <form action="welcome.php" method="POST">
 Name: <input type="text" name="name"><br />
--- 16_7/welcome.php
+++ 16_7/welcome.php
@@ -1,6 +1,10 @@
 <html>
+<head><link href="style.css" rel="stylesheet"></head>
+
 <body>
+<div class="one">
 Welcome <?php echo $_POST["name"]; ?><br />
 Your e-mail address is: <?php echo $_POST["email"]; ?>
+</div>
 </body>
 </html>
