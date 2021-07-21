a7f96ae8fb8f988c83088d996d59e89d7fa1ec8c
TienHanh2903 <63018227+TienHanh2903@users.noreply.github.com>
2021-07-22 05:18:41 +0700

    Update index1.php

--- 21_7/02/tra từ điển AV/index1.php
+++ 21_7/02/tra từ điển AV/index1.php
@@ -4,26 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Từ điển A-V</title>
 </head>
 <body>
-    <form action="1.php" method="POST">
+    <form action="#" method="POST">
         <label>Từ Tiếng Anh </label><!--nhập từ cần dịch-->
         <input type="text" name="engLish">
         <button>Dịch</button>
     </form>
     <div class="result">
     <?php 
-        $engLish= $_POST("engLish");
-           $vietNam = array("english"=>"tiếng anh","name"=>"tên","year"=>"năm","day"=>"tháng");
-           if(!empty($vietNam)){
-               foreach($vietNam as $key => $value){
+        if (isset($_POST["engLish"])) {
+        $engLish= $_POST["engLish"];
+        $tuDien= array("english"=>"tiếng anh","name"=>"tên","year"=>"năm","day"=>"tháng");
+           if(!empty($tuDien)){
+               foreach($tuDien as $key => $value){
                    if($key==$engLish){
-                       $hienThi= $key ." : " . $value;
+                       $hienThi= "<table><tr><td>Tiếng Anh</td><td>Tiếng Việt</td></tr><tr><td>". $key ."</td><td>" . $value ."</td></tr></table>";
                    }
                }
                echo $hienThi;
-           }
+           }}
+           
         ?>
     </div>
 </body>
